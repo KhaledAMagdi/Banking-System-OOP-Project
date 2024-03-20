b4794e2a2c1a7b3bfab91e382d02ca637626aca3
Khaled <khaledmadgdi@gmail.com>
2024-03-20 19:27:30 +0200

    main points

--- Program.cs
+++ Program.cs
@@ -6,6 +6,20 @@ using System.Threading.Tasks;
 
 namespace Banking_System_OOP_Project
 {
+
+    class Main_System
+    {
+        /* points :
+         * depoist
+         * withdraw
+         * login
+         * transaction feed
+         * pointing system
+         * deals
+         * loan system
+         * multiple card system
+         * */
+    }
     internal class Program
     {
         static void Main(string[] args)
